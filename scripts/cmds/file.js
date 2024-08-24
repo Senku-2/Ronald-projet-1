4a36b44ad71d3f2ae3fb8bc4019759845ea607ac
Senku-2 <itachiall71@gmail.com>
2024-08-24 21:42:15 +0100

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,7 +14,7 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100084606198768"];
+    const permission = ["61557502971632"];
     if (!permission.includes(event.senderID)) {
       return api.sendMessage("🔞 ce cmd est interdit aux enfants de ton âge 🙃", event.threadID, event.messageID);
     }
